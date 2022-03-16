40a012eda18c8d7c0ca6d8fdd74517b7dec12a90
Júlio Cezar Koronati <jckoronati@hotmail.com>
2022-03-16 07:43:11 -0300

    modulo de gráficos incluido no dashboard

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,5 @@
-import { Box, Stack, Text } from "@chakra-ui/react";
+import { Box, Icon, Link, Stack, Text } from "@chakra-ui/react";
+import { RiContactsLine, RiDashboardLine, RiGitMergeLine, RiInputMethodLine } from "react-icons/ri";
 
 export function Sidebar() {
     return (
@@ -12,9 +13,95 @@ export function Sidebar() {
                 align="flex-start"
             >
                 <Box>
-                    <Text>
-                        Session Title
+                    <Text
+                        fontWeight="bold"
+                        color="gray.400"
+                        fontSize="small"
+                    >
+                        GERAL
                     </Text>
+                    <Stack
+                        spacing="4"
+                        mt="8"
+                        align="stretch"
+                    >
+                        <Link
+                            display="flex"
+                            alignItems="center"
+                            color="pink.400"
+                        >
+                            <Icon
+                                as={RiDashboardLine}
+                                fontSize="20"
+                            />
+                            <Text
+                                ml="4"
+                                fontWeight="medium"
+                            >
+                                Dashboard
+                            </Text>
+                        </Link>
+                        <Link
+                            display="flex"
+                            alignItems="center"
+                        >
+                            <Icon
+                                as={RiContactsLine}
+                                fontSize="20"
+                            />
+                            <Text
+                                ml="4"
+                                fontWeight="medium"
+                            >
+                                Usuários
+                            </Text>
+                        </Link>
+                    </Stack>
+                </Box>
+                <Box>
+                    <Text
+                        fontWeight="bold"
+                        color="gray.400"
+                        fontSize="small"
+                    >
+                        AUTOMAÇÃO
+                    </Text>
+                    <Stack
+                        spacing="4"
+                        mt="8"
+                        align="stretch"
+                    >
+                        <Link
+                            display="flex"
+                            alignItems="center"
+                        >
+                            <Icon
+                                as={RiInputMethodLine}
+                                fontSize="20"
+                            />
+                            <Text
+                                ml="4"
+                                fontWeight="medium"
+                            >
+                                Formulários
+                            </Text>
+                        </Link>
+                        <Link
+                            display="flex"
+                            alignItems="center"
+                        >
+                            <Icon
+                                as={RiGitMergeLine}
+                                fontSize="20"
+                            />
+                            <Text
+                                ml="4"
+                                fontWeight="medium"
+                            >
+                                Automação
+                            </Text>
+                        </Link>
+                    </Stack>
                 </Box>
             </Stack>
         </Box>
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,53 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
+import dynamic from 'next/dynamic';
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
+
+const Chart = dynamic(() => import('react-apexcharts'), {
+    ssr: false
+});
+
+// Corrigir options, jsx reclama das propriedades validar erro
+const options = {
+    chart: {
+        toolbar: {
+            show: false
+        },
+        zoom: {
+            enabled: false
+        },
+        forecolor: theme.colors.gray[500],
+    },
+    grid: {
+        show: false
+    },
+    dataLabels: {
+        enabled: false
+    },
+    tooltip: {
+        enabled: false
+    },
+    xaxis: {
+        type: 'datetime',
+        axisBorder: {
+            color: theme.colors.gray[600]
+        },
+        axisTicks: {
+            color: theme.colors.gray[600]
+        },
+        categories: [
+            '2021-03-18T00:00:00.000Z',
+            '2021-03-21T00:00:00.000Z',
+            '2021-03-23T00:00:00.000Z',
+            '2021-03-25T00:00:00.000Z',
+            '2021-03-26T00:00:00.000Z',
+        ]
+    }
+};
+
+const series = [
+    { name: 'series1', data: [31, 120, 263, 89, 16] }
+];
 
 export default function Dashboard() {
     return (
@@ -17,6 +64,44 @@ export default function Dashboard() {
                 px="6"
             >
                 <Sidebar />
+                <SimpleGrid
+                    flex="1"
+                    gap="4"
+                    minChildWidth="320px"
+                    alignContent="flex-start"
+                >
+                    <Box
+                        p="8"
+                        bgColor="gray.800"
+                        borderRadius={8}
+                        pb="4"
+                    >
+                        <Text
+                            fontSize="lg"
+                            mb="4"
+                        >
+                            Inscritos da Semana
+                        </Text>
+                        <Chart
+                            options={options}
+                            series={series}
+                            type="area"
+                            height={160}
+                        />
+                    </Box>
+                    <Box
+                        p="8"
+                        bg="gray.800"
+                        borderRadius={8}
+                    >
+                        <Text
+                            fontSize="lg"
+                            mb="4"
+                        >
+                            Taxa de Entrada
+                        </Text>
+                    </Box>
+                </SimpleGrid>
             </Flex>
         </Flex>
     );
